c4c64d85dc51dc84f846096105a01416a8c30769
hariprasad <hariprasadnomula92@gmail.com>
2024-04-08 23:13:06 +0530

    by hari initial commit framework

---
+++ src/main/java/com/BDD/endtoend/App.java
@@ -0,0 +1,13 @@
+package com.BDD.endtoend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
